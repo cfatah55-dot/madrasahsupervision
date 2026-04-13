2a40d99a2d5c24f3e73c7a980eb271f2a826101f
cfatah55-dot <cfatah55@gmail.com>
2026-04-13 11:40:12 +0700

    Delete madrasah_supervision directory

--- madrasah_supervision/supervision/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class SupervisionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'supervision'
-    verbose_name = 'Manajemen Pengawasan Madrasah'
